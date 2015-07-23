2c8d03eace8944dc3644312261d3c716d53c65e6
osense <krupicka.adam@gmail.com>
2015-07-23 12:19:40 +0200

    Initial commit. Insert and lookup funs seem to be working.

---
+++ src/retrie.erl
@@ -0,0 +1,42 @@
+-module(retrie).
+
+-export([new/0, insert/3, lookup/2]).
+
+
+new() ->
+    {<<>>, undefined}.
+
+
+insert(<<H:8, T/binary>>, Value, Tree) ->
+    insert1(H, T, Value, Tree).
+
+insert1(H, <<>>, Value, {Lo, Hi, NodeVal, Array}) ->
+    NewTuple = case array:get(H, Array) of
+        {Lo1, Hi1, _, Array1} -> {Lo1, Hi1, Value, Array1};
+        {Key1, Value1} ->
+            NewNode = {H, H, Value, array:new()},
+            insert(Key1, Value1, NewNode)
+    end,
+    {Lo, Hi, NodeVal, array:set(H, NewTuple, Array)};
+insert1(H, T, Value, {Lo, Hi, NodeVal, Array}) ->
+    NewTuple = insert(T, Value, array:get(H, Array)),
+    {min(H, Lo), max(H, Hi), NodeVal, array:set(H, NewTuple, Array)};
+insert1(H, T, Value, {<<NH:8, NT/binary>>, NodeVal}) ->
+    NewNode = {NH, NH, none, array:set(NH, {NT, NodeVal}, array:new())},
+    insert1(H, T, Value, NewNode);
+insert1(H, T, Value, _) ->
+    {<<H:8, T/binary>>, Value}.
+
+
+lookup(<<H:8, T/binary>>, Tree) ->
+    lookup1(H, T, Tree);
+lookup(<<>>, {_, _, Value, _}) ->
+    Value.
+
+lookup1(H, T, {Lo, Hi, _, Array}) when H >= Lo, H =< Hi ->
+    lookup(T, array:get(H, Array));
+lookup1(H, T, {<<H:8, T/binary>>, Value}) ->
+    Value;
+lookup1(_, _, _) ->
+    undefined.
+
---
+++ test/retrie_tests.erl
@@ -0,0 +1,22 @@
+-module(retrie_tests).
+-author("osense").
+
+-include_lib("eunit/include/eunit.hrl").
+
+
+lookup_test() ->
+    T1 = retrie:new(),
+    T2 = retrie:insert(<<"abc">>, val1, T1),
+    ?assertEqual(val1, retrie:lookup(<<"abc">>, T2)),
+    T3 = retrie:insert(<<"a">>, val2, T2),
+    T4 = retrie:insert(<<"baca">>, val3, T3),
+    T5 = retrie:insert(<<"abced">>, val4, T4),
+    ?assertEqual(val2, retrie:lookup(<<"a">>, T5)),
+    ?assertEqual(val3, retrie:lookup(<<"baca">>, T5)),
+    ?assertEqual(val4, retrie:lookup(<<"abced">>, T5)),
+    T6 = retrie:insert(<<"a">>, val5, T5),
+    T7 = retrie:insert(<<"b">>, val6, T6),
+    T8 = retrie:insert(<<"abcabc">>, val7, T7),
+    ?assertEqual(val5, retrie:lookup(<<"a">>, T8)),
+    ?assertEqual(val6, retrie:lookup(<<"b">>, T8)),
+    ?assertEqual(val7, retrie:lookup(<<"abcabc">>, T8)).
