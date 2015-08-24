6d2ba45d81857175b5344588d51285519a69b3b5
osense <krupicka.adam@gmail.com>
2015-08-24 17:02:23 +0200

    Unicode tests.

--- test/retrie_tests.erl
+++ test/retrie_tests.erl
@@ -26,8 +26,8 @@ lookup_test() ->
 
 match_test() ->
     T0 = retrie:insert(<<"Hello">>, val, retrie:new()),
-    T1 = retrie:insert_pattern(<<"%{STRING:name}, hello!">>, T0),
-    ?assertEqual([{<<"name">>, <<"World">>}], retrie:lookup_match(<<"World, hello!">>, T1)),
+    T1 = retrie:insert_pattern(<<"%{STRING:name}, helló!"/utf8>>, T0),
+    ?assertEqual([{<<"name">>, <<"World">>}], retrie:lookup_match(<<"World, helló!"/utf8>>, T1)),
     ?assertEqual(nomatch, retrie:lookup_match(<<"World, World!">>, T1)),
     T11 = retrie:insert_pattern(<<"Hello, %{STRING:name}">>, T1),
     ?assertEqual([{<<"name">>, <<"hello">>}], retrie:lookup_match(<<"Hello, hello">>, T11)),
@@ -37,6 +37,6 @@ match_test() ->
     T2 = retrie:insert_pattern(<<"Hello %{STRING:name} id: %{INT:id}">>, T1),
     ?assertEqual([{<<"name">>, <<"Foo">>}, {<<"id">>, 34}], retrie:lookup_match(<<"Hello Foo id: 34">>, T2)),
     T3 = retrie:insert_pattern(<<"Hello %{STRING:name} %{BOOL:b}">>, T2),
-    ?assertEqual([{<<"name">>, <<"Foo">>}, {<<"b">>, false}], retrie:lookup_match(<<"Hello Foo false">>, T3)),
+    ?assertEqual([{<<"name">>, <<"Fôô"/utf8>>}, {<<"b">>, false}], retrie:lookup_match(<<"Hello Fôô false"/utf8>>, T3)),
     ?assertEqual(nomatch, retrie:lookup_match(<<"World, World!">>, T3)),
     ?assertEqual(nomatch, retrie:lookup_match(<<"Hello Foo nobool">>, T3)).
